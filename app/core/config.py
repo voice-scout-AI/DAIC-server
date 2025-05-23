dd212413dc13f49a09b4396d42e106c55455c96b
philosophia301 <eunjong147@gmail.com>
2025-05-23 14:58:13 +0900

    Initial setting

---
+++ app/core/config.py
@@ -0,0 +1,7 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+UPSTAGE_API_KEY = os.getenv("UPSTAGE_API_KEY")
---
+++ app/services/document_parser.py
@@ -0,0 +1,3 @@
+from ..core.config import UPSTAGE_API_KEY
+
+print(UPSTAGE_API_KEY)
